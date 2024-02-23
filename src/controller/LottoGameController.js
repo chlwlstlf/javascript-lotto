7e636bc8504d1ebe7e4fb347946036fa56401155
jinsil <jinsil378900@gmail.com>
2024-02-23 17:51:04 +0900

    fix: reduce 할당문 수정

--- src/controller/LottoGameController.js
+++ src/controller/LottoGameController.js
@@ -36,8 +36,7 @@ class LottoGameController {
     const totalProfit = Object.entries(winningResults).reduce((profit, [matchedKey, count]) => {
       return profit + RANKING[matchedKey].REWARD * count;
     }, 0);
-    return 100;
-    // return ((totalProfit * 100) / this.#purchaseAmount).toLocaleString('ko-KR', { minimumFractionDigits: 1 });
+    return ((totalProfit * 100) / this.#purchaseAmount).toLocaleString('ko-KR', { minimumFractionDigits: 1 });
   }
 
   #restartGame(restartCommand) {
